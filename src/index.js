d5350059b76e793969063e5cfd5c1c584cf31bbb
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-06-18 02:21:52 +0300

    commit tree 4269a6a1e009dda9591c359ad65d42bb88fe5f23

--- src/index.js
+++ src/index.js
@@ -13,3 +13,70 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+document.querySelector('.add-toy-form').addEventListener('submit',toSubmit)
+
+function toSubmit(e) {
+  e.preventDefault()
+  let newToy={
+    name:e.target.name.value,
+    image:e.target.image.value,
+    likes:0
+  }
+  addToys(newToy)
+  
+}
+
+function addToys(newToy){
+  fetch('http://localhost:3000/toys',{
+    method: 'POST',
+    headers: 
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body:JSON.stringify(newToy)
+  })
+  .then(res => res.json())
+  .then(toy => console.log(toy))
+}
+
+function displayCharacters(character){
+  let card = document.createElement('div')
+    card.className = 'card'
+    card.innerHTML = `
+        <h2>${character.name}</h2>
+        <img src="${character.image}"  class="toy-avatar" />
+        <p>
+        <span class = "likes-count">${character.likes} </span> Likes </p>
+        <button class="like-btn" id="[toy_id]">Like</button>
+        `
+      card.querySelector('.like-btn').addEventListener('click',() => {
+        character.likes+= 1
+        card.querySelector('span').textContent = character.likes
+      } )
+        document.querySelector('#toy-collection').appendChild(card)
+  }
+
+  function updateToys(newToy){
+    fetch(`http://localhost:3000/toys/${newToy.id}`,{
+      method: 'PATCH',
+      headers: 
+      {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body:JSON.stringify(newToy)
+    })
+    .then(res => res.json())
+    .then(toy => console.log(toy))
+  }
+  
+  getAllToys()
+
+  function getAllToys(){
+    fetch('http://localhost:3000/toys')
+    .then(res => res.json())
+    .then(toyData => toyData.forEach(toy => displayCharacters(toy) ))
+    
+  }
